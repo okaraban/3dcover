8e176b2b1dd6e2fff959fdc7503ddfc8b6bb2b76
Kyrylo <kyrylo.onufriiev@gmail.com>
2017-12-15 01:57:25 +0200

    email notification

---
+++ server/utils/Mailer.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+const handlebars = require('handlebars');
+const fs = require('fs-promise');
+const { email } = require('../config/config.json');
+
+const noreply = nodemailer.createTransport({
+  service: 'gmail',
+  auth: email
+});
+
+const subjects = {
+  thanks: 'Thank you for your order!'
+}
+
+const send = async ({ to, subject, template, data }) => {
+  const hbs = await fs.readFile(`./server/emails/${template}.hbs`, 'utf8');
+  const model = handlebars.compile(hbs);
+  const info = await noreply.sendMail({
+    from: `"Wrap" <${email.user}>`,
+    to,
+    subject: subjects[template],
+    html: model(data)
+  });
+  return info;
+};
+
+module.exports = { send };
