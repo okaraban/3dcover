2f6217b65b20eafca0abe879ac65c089cf5194f7
Kyrylo <kyrylo.onufriiev@gmail.com>
2017-12-16 14:27:50 +0200

    fom validation

--- components/OrderForm.vue
+++ components/OrderForm.vue
@@ -2,23 +2,23 @@
   <div class="fluid wrapper">
     <el-button type="primary" icon="fa fa-paper-plane" @click="dialogFormVisible = true" class="fluid"> Send to the Server </el-button>
     <el-dialog title="Send to the Server" :visible.sync="dialogFormVisible">
-      <el-form :model="form">
-        <el-form-item label="First name">
-          <el-input v-model="form.firstname" auto-complete="off"></el-input>
+      <el-form>
+        <el-form-item :label="firstnameLabel" :class="{ 'error': $v.form.firstname.$error }">
+          <el-input v-model="form.firstname" auto-complete="off" @input="$v.form.firstname.$touch()"></el-input>
         </el-form-item>
-        <el-form-item label="Last name">
-          <el-input v-model="form.lastname" auto-complete="off"></el-input>
+        <el-form-item :label="lastnameLabel" :class="{ 'error': $v.form.lastname.$error }">
+          <el-input v-model="form.lastname" auto-complete="off" @input="$v.form.lastname.$touch()"></el-input>
         </el-form-item>
-        <el-form-item label="Phone number">
-          <el-input v-model="form.phone" auto-complete="off"></el-input>
+        <el-form-item :label="phoneLabel" :class="{ 'error': $v.form.phone.$error }">
+          <el-input v-model="form.phone" auto-complete="off" @input="$v.form.phone.$touch()"></el-input>
         </el-form-item>
-        <el-form-item label="Email">
-          <el-input v-model="form.email" auto-complete="off"></el-input>
+        <el-form-item :label="emailLabel" :class="{ 'error': $v.form.email.$error }">
+          <el-input v-model="form.email" auto-complete="off" @input="$v.form.email.$touch()"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="confirm">Confirm</el-button>
+        <el-button type="primary" :disabled="$v.form.$invalid" @click="confirm">Confirm</el-button>
       </span>
     </el-dialog>
   </div>
@@ -26,6 +26,7 @@
 
 <script>
   import axios from 'axios'
+  import { required, alpha, numeric, email } from 'vuelidate/lib/validators'
 
   export default {
     name: 'OrderForm',
@@ -43,11 +44,62 @@
         }
       };
     },
+    validations: {
+      form: {
+        firstname: {
+          required,
+          alpha
+        },
+        lastname: {
+          required,
+          alpha
+        },
+        phone: {
+          numeric
+        },
+        email: {
+          required,
+          email
+        }
+      }
+    },
+    computed: {
+      firstnameLabel() {
+        const firstname = "First name";
+        return !this.$v.form.firstname.$error ? `${firstname}*` :
+          !this.$v.form.firstname.required ? `${firstname} is required` :
+          !this.$v.form.firstname.alpha ? `${firstname} must contain only letters` :
+          `${firstname} is invalid`;
+      },
+      lastnameLabel() {
+        const lastname = "Last name";
+        return !this.$v.form.lastname.$error ? `${lastname}*` :
+          !this.$v.form.lastname.required ? `${lastname} is required` :
+          !this.$v.form.lastname.alpha ? `${lastname} must contain only letters` :
+          `${lastname} is invalid`;
+      },
+      phoneLabel() {
+        const phone = "Phone number";
+        return !this.$v.form.phone.$error ? phone :
+          !this.$v.form.phone.numeric ? `${phone} must contain only numbers` :
+          `${phone} is invalid`;
+      },
+      emailLabel() {
+        const email = "Email";
+        return !this.$v.form.email.$error ? `${email}*` :
+          !this.$v.form.email.required ? `${email} is required` :
+          !this.$v.form.email.email ? `${email} must be formated` :
+          `${email} is invalid`;
+      }
+    },
     methods: {
       async confirm() {
         try {
           const res = await axios.post('/api/upload', {
-            ...this.form,
+            firstname: this.firstname,
+            lastname: this.lastname,
+            phone: this.phone,
+            email: this.email,
             source: this.source()
           });
           this.dialogFormVisible = false;
@@ -65,5 +117,11 @@
   }
   .wrapper {
     margin-top: 10px;
+  }
+  .error .el-input__inner {
+    border-color: #f79483;
+  }
+  .error .el-form-item__label {
+    color: #f04124;
   }
 </style>
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import ElementUI from 'element-ui'
 import App from './components/App.vue'
 import locale from 'element-ui/lib/locale/lang/en'
@@ -6,6 +7,7 @@ import locale from 'element-ui/lib/locale/lang/en'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/font-awesome.css'
 
+Vue.use(Vuelidate)
 Vue.use(ElementUI, { locale })
 
 const vue = new Vue({
